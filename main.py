fb5315fea77f96e5c48d1647b78f8209bd38d880
levyn96 <levyn96@gmail.com>
2021-04-16 21:51:06 +0300

    returning json instead of string

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from kubernetes import client, config
 from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_query_string():
-    return check_pods(request.query_string.decode()).encode()
+    return check_pods(request.query_string.decode())
 
 def check_pods(req_ip):
+    ans_json = []
     config.load_incluster_config()
     v1 = client.CoreV1Api()
     print("Listing pods with their IPs:")
@@ -16,10 +18,9 @@ def check_pods(req_ip):
         print("%s\t%s\t%s" %
               (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
         if i.status.pod_ip == req_ip:
-            print("pod name: {}".format(i.metadata.name))
-            print("pod namespace: {}".format(i.metadata.namespace))
-            return "name: {}, namespace: {}".format(i.metadata.name, i.metadata.namespace)
-    return req_ip+" not found"
+            ans_json.append({"name": i.metadata.name, "namespace": i.metadata.namespace})
+    data = json.dumps(ans_json, indent=2)
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- server-test.py
+++ server-test.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
@@ -6,7 +7,8 @@ app = Flask(__name__)
 def get_query_string():
     param = request.query_string
     print(type(param))
-    ans = param.decode() + " decoding works"
-    return ans.encode()
+    ans = [{"task_name":param.decode(),"task_info":"aaaaa"},{"task_name":"task2","task_info":"bbbbb"}]
+    data = json.dumps(ans, indent=2)
+    return data
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
